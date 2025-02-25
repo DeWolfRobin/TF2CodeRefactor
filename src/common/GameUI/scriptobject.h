b6798bdd84c89a06b509e4a7e052b2a97bbbfa7f
Robin De Wolf <rdewolf@telenet.be>
2025-02-25 11:54:43 +0100

    common

--- src/common/GameUI/scriptobject.h
+++ src/common/GameUI/scriptobject.h
@@ -3,7 +3,7 @@
 // Purpose: 
 //
 // $NoKeywords: $
-//=============================================================================//
+//=============================================================================
 
 #ifndef SCRIPTOBJECT_H
 #define SCRIPTOBJECT_H
@@ -12,145 +12,152 @@
 #endif
 
 #include <vgui_controls/Panel.h>
+#include <initializer_list>
 
 class CPanelListPanel;
 
 #define SCRIPT_VERSION 1.0f
 
-typedef void * FileHandle_t;
+typedef void* FileHandle_t;
 
 enum objtype_t
 {
-	O_BADTYPE,
-	O_BOOL,
-	O_NUMBER,
-	O_LIST,
-	O_STRING,
-	O_OBSOLETE,
-	O_SLIDER,
-	O_CATEGORY,
-	O_BUTTON,
+    O_BADTYPE,
+    O_BOOL,
+    O_NUMBER,
+    O_LIST,
+    O_STRING,
+    O_OBSOLETE,
+    O_SLIDER,
+    O_CATEGORY,
+    O_BUTTON,
 };
 
 typedef struct
 {
-	objtype_t type;
-	char szDescription[32];
+    objtype_t type;
+    char szDescription[32];
 } objtypedesc_t;
 
 class CScriptListItem
 {
 public:
-	CScriptListItem();
-	CScriptListItem( char const *strItem, char const *strValue );
+    CScriptListItem();
+    CScriptListItem(char const* strItem, char const* strValue);
 
-	char szItemText[128];
-	char szValue[256];
+    char szItemText[128];
+    char szValue[256];
 
-	CScriptListItem *pNext;
+    CScriptListItem* pNext;
 };
 
 class CScriptObject : public vgui::Panel
 {
 public:
-	void AddItem( CScriptListItem *pItem );
-	void RemoveAndDeleteAllItems( void );
-	CScriptObject( void );
-	~CScriptObject();
+    CScriptObject(void);
+    ~CScriptObject();
 
-	bool ReadFromBuffer( const char **pBuffer, bool isNewObject );
-	void WriteToConfig();
-	void WriteToFile( FileHandle_t fp );
-	void WriteToScriptFile( FileHandle_t fp );
-	void SetCurValue( char const *strValue );
+    void AddItem(CScriptListItem* pItem);
+    void RemoveAndDeleteAllItems(void);
+    bool ReadFromBuffer(const char** pBuffer, bool isNewObject);
+    void WriteToConfig();
+    void WriteToFile(FileHandle_t fp);
+    void WriteToScriptFile(FileHandle_t fp);
+    void SetCurValue(char const* strValue);
 
-	objtype_t GetType( char *pszType );
+    objtype_t GetType(char* pszType);
 
-	objtype_t type;
+    objtype_t type;
 
-	char cvarname[64 ];
-	char prompt[ 256 ];
-	char tooltip[ 256 ];
+    char cvarname[64];
+    char prompt[256];
+    char tooltip[256];
 
-	CScriptListItem *pListItems;
+    CScriptListItem* pListItems;
 
-	float fMin, fMax;
+    float fMin, fMax;
 
-	char	defValue[ 128 ];  // Default value string
-	float   fdefValue; // Float version of default value.
+    char defValue[128];  // Default value string
+    float fdefValue;     // Float version of default value.
 
-	char	curValue[ 128 ];
-	float   fcurValue;
+    char curValue[128];
+    float fcurValue;
 
-	bool bSetInfo;  // Prepend "Setinfo" to keyvalue pair in config?
-	// Linked list of default list box items.
+    bool bSetInfo;       // Prepend "Setinfo" to keyvalue pair in config?
+    // Linked list of default list box items.
+    CScriptObject* pNext;
 
-	CScriptObject *pNext;
+private:
+    // Tail pointer to quickly append new list items.
+    CScriptListItem* m_pLastItem;
 };
 
 abstract_class CDescription
 {
 public:
-	CDescription( void );
-	virtual ~CDescription();
+    CDescription(void);
+    virtual ~CDescription();
 
-	bool ReadFromBuffer( const char **pBuffer, bool bAllowNewObject );
-	bool InitFromFile( const char *pszFileName, bool bAllowNewObject = true );
-	void TransferCurrentValues( const char *pszConfigFile );
+    bool ReadFromBuffer(const char** pBuffer, bool bAllowNewObject);
+    bool InitFromFile(const char* pszFileName, bool bAllowNewObject = true);
+    void TransferCurrentValues(const char* pszConfigFile);
 
-	void AddObject( CScriptObject *pItem );
-	void WriteToConfig();
-	void WriteToFile( FileHandle_t fp );
-	void WriteToScriptFile( FileHandle_t fp );
+    void AddObject(CScriptObject* pItem);
+    void WriteToConfig();
+    void WriteToFile(FileHandle_t fp);
+    void WriteToScriptFile(FileHandle_t fp);
 
-	virtual void WriteScriptHeader( FileHandle_t fp ) = 0; // Clients must implement this.
-	virtual void WriteFileHeader( FileHandle_t fp ) = 0; // Clients must implement this.
+    virtual void WriteScriptHeader(FileHandle_t fp) = 0; // Clients must implement this.
+    virtual void WriteFileHeader(FileHandle_t fp) = 0;   // Clients must implement this.
 
-	void setDescription( const char *pszDesc );
-	void setHint( const char *pszHint );
+    void setDescription(const char* pszDesc);
+    void setHint(const char* pszHint);
 
-	const char *GetDescription( void ) { return m_pszDescriptionType; };
-	const char *getHint( void ) { return m_pszHintText; } ;
+    const char* GetDescription(void) { return m_pszDescriptionType; }
+    const char* getHint(void) { return m_pszHintText; }
+
 public:
-	CScriptObject *pObjList;
-	CScriptObject *FindObject( const char *pszObjectName );
+    CScriptObject* pObjList;
+    CScriptObject* FindObject(const char* pszObjectName);
 
 private:
+    // Tail pointer to quickly append new objects.
+    CScriptObject* m_pLastObj;
 
-	char *m_pszHintText;
-	char *m_pszDescriptionType;
+    char* m_pszHintText;
+    char* m_pszDescriptionType;
 };
 
 namespace vgui
 {
-	class Label;
-	class Panel;
+    class Label;
+    class Panel;
 }
 
 class mpcontrol_t : public vgui::Panel
 {
 public:
-	mpcontrol_t( vgui::Panel *parent, char const *panelName );
+    mpcontrol_t(vgui::Panel* parent, char const* panelName);
 
-	virtual	void	OnSizeChanged( int wide, int tall ) OVERRIDE;
+    virtual void OnSizeChanged(int wide, int tall) OVERRIDE;
 
-	objtype_t		type;
-	vgui::Panel		*pControl;
-	vgui::Label		*pPrompt;
-	CScriptObject	*pScrObj;
+    objtype_t type;
+    vgui::Panel* pControl;
+    vgui::Label* pPrompt;
+    CScriptObject* pScrObj;
 
-	mpcontrol_t		*next;
+    mpcontrol_t* next;
 };
 
 class CInfoDescription : public CDescription
 {
 public:
-	CInfoDescription( void );
+    CInfoDescription(void);
 
-	virtual void WriteScriptHeader( FileHandle_t fp ) OVERRIDE;
-	virtual void WriteFileHeader( FileHandle_t fp ) OVERRIDE; 
+    virtual void WriteScriptHeader(FileHandle_t fp) OVERRIDE;
+    virtual void WriteFileHeader(FileHandle_t fp) OVERRIDE;
 };
 
-void UTIL_StripInvalidCharacters( char *pszInput, int maxlen );
+void UTIL_StripInvalidCharacters(char* pszInput, int maxlen);
 
 #endif // SCRIPTOBJECT_H
